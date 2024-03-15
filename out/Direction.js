c202deb5cb9e4582386bdc88e41a53a1cb9f1b4b
Wayne Howarth <wayne.howarth@icloud.com>
2024-03-15 19:47:16 +0000

    Created bundle

--- out/Direction.js
+++
@@ -1,9 +0,0 @@
-var Direction;
-(function (Direction) {
-    Direction[Direction["None"] = 0] = "None";
-    Direction[Direction["North"] = 1] = "North";
-    Direction[Direction["East"] = 2] = "East";
-    Direction[Direction["South"] = 4] = "South";
-    Direction[Direction["West"] = 8] = "West";
-})(Direction || (Direction = {}));
-//# sourceMappingURL=Direction.js.map
